19d6c9d63486547c80ea5be23d61a140bd9ae7eb
OscarPerez1992 <oscar.perez2@alu.uclm.es>
2018-03-20 08:37:07 +0100

    Update Greeter.java

--- server/src/main/java/com/example/Greeter.java
+++ server/src/main/java/com/example/Greeter.java
@@ -13,8 +13,9 @@ public class Greeter {
   }
 
  /**
- * Comentario1
- **/
+ * @param nombre de persona
+ * @return string greeting
+ */
   public final String greet(final String someone) {
     return String.format("Hello, %s!", someone);
   }
